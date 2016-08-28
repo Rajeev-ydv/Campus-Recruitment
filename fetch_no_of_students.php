75a43d88d29c9077226fab45624a012aa4818e57
Rajeev Yadav <rajeevyadav971@gmail.com>
2016-08-28 17:06:19 +0530

    php to fetch no of eligible students

---
+++ fetch_no_of_students.php
@@ -0,0 +1,33 @@
+<?php
+				$hs_marks="";
+				$ss_marks="";
+				$graduation_marks="";
+				
+				$hs_marks = $_GET["hs_marks"];
+				$ss_marks = $_GET["ss_marks"];
+				$graduation_marks = $_GET["graduation_marks"];
+				
+				
+				$conn = new mysqli("localhost", "root", "", "campusfinale");
+				if ($conn->connect_error) {
+					echo "Connection failed: " . $conn->connect_error;
+				} 
+				else
+				{
+					$sql = 'select * from student where HS >=';
+					$sql.=$hs_marks;
+					$sql.=' and SS >=';
+					$sql.=$ss_marks;
+					$sql.=' and GRADUATION >=';
+					$sql.=$graduation_marks;
+					$sql.=';';
+					
+					$result = $conn->query($sql);
+					if($result->num_rows > 0)
+					{
+							echo $result->num_rows;
+					}
+				}
+				
+				
+			?>
